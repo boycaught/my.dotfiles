8e25c833464f39802bd16d168bdee766d08ff23b
LAG <github.robot@lagtime.com>
2020-08-24 14:53:46 -0700

    Create base_osx__homebrew_installs.sh

---
+++ base_osx__homebrew_installs.sh
@@ -0,0 +1,171 @@
+#!/bin/sh
+
+########## binaries
+brew install ack 
+brew install android-sdk 
+brew install ansible 
+brew install ant 
+brew install apple-gcc42 
+brew install aspell 
+brew install augeas 
+brew install autoconf 
+brew install automake 
+brew install awscli 
+brew install bash 
+brew install boost 
+brew install dialog 
+brew install dvm 
+brew install cmake 
+brew install composer 
+brew install consul 
+brew install coreutils 
+brew install certbot 
+brew install dockutil 
+brew install doxygen 
+brew install dnsmasq
+brew install dvm 
+brew install elasticsearch 
+brew install elixir 
+brew install elm 
+brew install emscripten 
+brew install enchant 
+brew install erlang 
+brew install faac 
+brew install ffmpeg 
+brew install findutils 
+brew install freetype 
+brew install gdbm 
+brew install gettext 
+brew install git 
+brew install glib 
+brew install glide 
+brew install gmp 
+brew install gnupg 
+brew install go 
+brew install gradle 
+brew install grails 
+brew install grep 
+brew install groovy 
+brew install htop-osx 
+brew install httping 
+brew install icu4c 
+brew install imagemagick 
+brew install jenkins 
+brew install jetty 
+brew install jpeg 
+brew install jruby 
+brew install jsdoc3 
+brew install jython 
+brew install kubernetes-cli 
+brew install kubernetes-helm 
+brew install lame 
+brew install lastpass-cli 
+brew install libassuan 
+brew install libav 
+brew install libffi 
+brew install libgpg-error 
+brew install libidn 
+brew install libksba 
+brew install libpng 
+brew install libressl 
+brew install libsass 
+brew install libssh2 
+brew install libtiff 
+brew install libtool 
+brew install libxml2 
+brew install libyaml 
+brew install liquibase 
+brew install llvm 
+brew install makedepend 
+brew install mariadb 
+brew install mcrypt 
+brew install mercurial 
+brew install md5deep 
+brew install mhash 
+brew install mobile-shell 
+brew install mongodb 
+brew install mysql-sandbox 
+brew install mono 
+brew install neo4j 
+brew install nginx 
+brew install ninja 
+brew install node 
+brew install openssl 
+brew install packer 
+brew install pandoc 
+brew install passenger 
+brew install pcre 
+brew install php 
+brew install phpmyadmin 
+brew install pinentry 
+brew install pkg-config 
+brew install privoxy 
+brew install protobuf 
+brew install pyenv 
+brew install pyenv-virtualenv 
+brew install python 
+brew install rabbitmq 
+brew install readline 
+brew install redis 
+brew install rename 
+brew install rethinkdb 
+brew install ringojs 
+brew install rust 
+brew install s3cmd 
+brew install scons 
+brew install sassc 
+brew install sqlite 
+brew install terraform 
+brew install tidy-html5 
+brew install tomcat 
+brew install tree 
+brew install unixodbc 
+brew install wget 
+brew install wp-cli 
+brew install wxmac 
+brew install xz 
+brew install xvid 
+brew install x264 
+brew install youtube-dl 
+brew install yuicompressor 
+
+########## casks
+brew cask install audio-hijack
+brew cask install audioscrobbler
+brew cask install altair-graphql-client
+brew cask install db-browser-for-sqlite
+brew cask install firecamp
+brew cask install dotnet-sdk
+brew cask install gramps
+brew cask install keepassxc
+brew cask install libreoffice
+brew cask install thebrain
+brew cask install vienna
+brew cask install quiterss
+brew cask install zettlr
+brew cask install zettelkasten
+brew cask install atom 
+brew cask install brave 
+brew cask install codekit 
+brew cask install cyberduck 
+brew cask install docker 
+brew cask install electron 
+brew cask install freemind 
+brew cask install gimp 
+brew cask install fission
+brew cask install handbrake 
+brew cask install inkscape 
+brew cask install iterm2 
+brew cask install flycut
+brew cask install keybase 
+brew cask install mongotron 
+brew cask install sqlectron 
+brew cask install transmit
+brew cask install virtualbox 
+brew cask install vagrant 
+brew cask install vagrant-manager 
+brew cask install visual-studio-code 
+brew cask install vlc 
+brew cask install xquartz
+
+# v.2020.08.24
