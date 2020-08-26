6018e1e0c69500944fa8d7188b3ba0d6fa5bc627
LAG <github.robot@lagtime.com>
2020-08-25 23:04:12 -0700

    Update base_osx__homebrew_installs.sh
    Replaced line by line calls with array loops.

--- base_osx__homebrew_installs.sh
+++ base_osx__homebrew_installs.sh
@@ -1,178 +1,199 @@
 #!/bin/sh
 
-########## binaries
-brew install ack 
-brew install android-sdk 
-brew install ansible 
-brew install ant 
-brew install apple-gcc42 
-brew install aspell 
-brew install augeas 
-brew install autoconf 
-brew install automake 
-brew install awscli 
-brew install bash 
-brew install boost 
-brew install certbot 
-brew install clamav 
-brew install cmake 
-brew install composer 
-brew install consul 
-brew install coreutils 
-brew install dialog 
-brew install dnsmasq
-brew install dockutil 
-brew install doxygen 
-brew install dvm 
-brew install dvm 
-brew install elasticsearch 
-brew install elixir 
-brew install elm 
-brew install emscripten 
-brew install enchant 
-brew install erlang 
-brew install faac 
-brew install ffmpeg 
-brew install findutils 
-brew install freetype 
-brew install gdbm 
-brew install gettext 
-brew install git 
-brew install glib 
-brew install glide 
-brew install gmp 
-brew install gnupg 
-brew install go 
-brew install gradle 
-brew install grails 
-brew install grep 
-brew install groovy 
-brew install htop-osx 
-brew install httping 
-brew install icu4c 
-brew install imagemagick 
-brew install jenkins 
-brew install jetty 
-brew install jpeg 
-brew install jruby 
-brew install jsdoc3 
-brew install jupyter 
-brew install jython 
-brew install kubernetes-cli 
-brew install kubernetes-helm 
-brew install lame 
-brew install lastpass-cli 
-brew install libassuan 
-brew install libav 
-brew install libffi 
-brew install libgpg-error 
-brew install libidn 
-brew install libksba 
-brew install libpng 
-brew install libressl 
-brew install libsass 
-brew install libssh2 
-brew install libtiff 
-brew install libtool 
-brew install libxml2 
-brew install libyaml 
-brew install liquibase 
-brew install llvm 
-brew install makedepend 
-brew install mariadb 
-brew install mcrypt 
-brew install md5deep 
-brew install mercurial 
-brew install mhash 
-brew install mobile-shell 
-brew install mongodb 
-brew install mono 
-brew install mysql-sandbox 
-brew install neo4j 
-brew install nginx 
-brew install ninja 
-brew install node 
-brew install openssl 
-brew install packer 
-brew install pandoc 
-brew install passenger 
-brew install pcre 
-brew install php 
-brew install phpmyadmin 
-brew install pinentry 
-brew install pkg-config 
-brew install privoxy 
-brew install protobuf 
-brew install pyenv 
-brew install pyenv-virtualenv 
-brew install python 
-brew install rabbitmq 
-brew install r 
-brew install readline 
-brew install redis 
-brew install rename 
-brew install rethinkdb 
-brew install ringojs 
-brew install rust 
-brew install s3cmd 
-brew install sassc 
-brew install scons 
-brew install sqlite 
-brew install terraform 
-brew install tidy-html5 
-brew install tomcat 
-brew install tree 
-brew install unixodbc 
-brew install wget 
-brew install wp-cli 
-brew install wxmac 
-brew install x264 
-brew install xvid 
-brew install xz 
-brew install youtube-dl 
-brew install yuicompressor 
+echo "Install Homebrew software BINARIES and CASKS. ........\n"
 
-########## casks
-brew cask install altair-graphql-client
-brew cask install atom 
-brew cask install audio-hijack
-brew cask install audioscrobbler
-brew cask install codekit 
-brew cask install cyberduck 
-brew cask install db-browser-for-sqlite
-brew cask install docker 
-brew cask install dotnet-sdk
-brew cask install dropbox
-brew cask install electron 
-brew cask install firecamp
-brew cask install fission
-brew cask install flycut
-brew cask install freemind 
-brew cask install gimp 
-brew cask install gramps
-brew cask install handbrake 
-brew cask install homebrew/cask-versions/brave-browser-dev   
-brew cask install homebrew/cask-versions/firefox-developer-edition            
-brew cask install homebrew/cask-versions/google-chrome-dev
-brew cask install homebrew/cask-versions/safari-technology-preview
-brew cask install inkscape 
-brew cask install iterm2 
-brew cask install keepassxc
-brew cask install keybase 
-brew cask install libreoffice
-brew cask install mongotron 
-brew cask install quiterss
-brew cask install sqlectron 
-brew cask install thebrain
-brew cask install transmit
-brew cask install vagrant 
-brew cask install vagrant-manager 
-brew cask install vienna
-brew cask install virtualbox 
-brew cask install visual-studio-code 
-brew cask install vlc 
-brew cask install xquartz
-brew cask install zettelkasten
-brew cask install zettlr
+# put binaries into array
+binary_urls=(
+ack 
+android-sdk 
+ansible 
+ant 
+apple-gcc42 
+aspell 
+augeas 
+autoconf 
+automake 
+awscli 
+bash 
+boost 
+certbot 
+clamav 
+cmake 
+composer 
+consul 
+coreutils 
+dialog 
+dnsmasq
+dockutil 
+doxygen 
+dvm 
+dvm 
+elasticsearch 
+elixir 
+elm 
+emscripten 
+enchant 
+erlang 
+faac 
+ffmpeg 
+findutils 
+freetype 
+gdbm 
+gettext 
+git 
+glib 
+glide 
+gmp 
+gnupg 
+go 
+gradle 
+grails 
+grep 
+groovy 
+htop-osx 
+httping 
+icu4c 
+imagemagick 
+jenkins 
+jetty 
+joplin
+jpeg 
+jruby 
+jsdoc3 
+jupyter 
+jython 
+kubernetes-cli 
+kubernetes-helm 
+lame 
+lastpass-cli 
+libassuan 
+libav 
+libffi 
+libgpg-error 
+libidn 
+libksba 
+libpng 
+libressl 
+libsass 
+libssh2 
+libtiff 
+libtool 
+libxml2 
+libyaml 
+liquibase 
+llvm 
+makedepend 
+mariadb 
+mcrypt 
+md5deep 
+mercurial 
+mhash 
+mobile-shell 
+mongodb 
+mono 
+mysql-sandbox 
+neo4j 
+nginx 
+ninja 
+node 
+openssl 
+packer 
+pandoc 
+passenger 
+pcre 
+php 
+phpmyadmin 
+pinentry 
+pkg-config 
+postgres 
+privoxy 
+protobuf 
+pyenv 
+pyenv-virtualenv 
+python 
+rabbitmq 
+r 
+readline 
+redis 
+rename 
+rethinkdb 
+ringojs 
+rust 
+s3cmd 
+sassc 
+scons 
+sqlite 
+terraform 
+tidy-html5 
+tomcat 
+tree 
+unixodbc 
+wget 
+wp-cli 
+wxmac 
+x264 
+xvid 
+xz 
+youtube-dl 
+yuicompressor 
+)
+# loop through array
+for b in "${binary_urls[@]}"
+do
+	brew install $b
+done
+# report completion
+echo "..... BINARIES installed. ...................\n"
 
-# v.2020.08.25
+# put casks into array
+cask_urls=(
+altair-graphql-client
+atom 
+audio-hijack
+audioscrobbler
+codekit 
+cyberduck 
+db-browser-for-sqlite
+docker 
+dotnet-sdk
+dropbox
+electron 
+firecamp
+fission
+flycut
+freemind 
+gimp 
+gramps
+handbrake 
+homebrew/cask-versions/brave-browser-dev   
+homebrew/cask-versions/firefox-developer-edition            
+homebrew/cask-versions/google-chrome-dev
+homebrew/cask-versions/safari-technology-preview
+inkscape 
+iterm2 
+keepassxc
+keybase 
+libreoffice
+mongotron 
+quiterss
+sqlectron 
+thebrain
+transmit
+vagrant 
+vagrant-manager 
+vienna
+virtualbox 
+visual-studio-code 
+vlc 
+xquartz
+zettelkasten
+zettlr
+)
+# loop through array
+for c in "${cask_urls[@]}"
+do
+	brew cask install $c
+done
+
+# report completion
+echo "........ CASKS installed."
