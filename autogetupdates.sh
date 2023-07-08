d9ab01b6618dff040831c91979dc59911bc14938
LAG <github.robot@lagtime.com>
2023-07-08 12:25:29 -0700

    Update autogetupdates.sh

--- autogetupdates.sh
+++ autogetupdates.sh
@@ -19,7 +19,7 @@
 ### done
 
 # Run the streamline and getupdates programs
-streamline && getupdates && cbonsai
+streamline && getupdates
 
 # Send an email when the program is done
 echo "getupdates has completed" | mail -s "getupdates completed" dev.bot@lagtime.com
